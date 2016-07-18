fb12f6f1fc427102eea08f2006e8c2183ab41f6b
Rikhard Gildman <rikhard.gildman@noveogroup.com>
2016-07-18 10:18:39 +0600

    Updated apidoc

--- api/web/doc/api_project.js
+++ api/web/doc/api_project.js
@@ -2,7 +2,7 @@ define({
   "name": "2GIS API",
   "version": "1.0.0",
   "description": "2GIS REST API",
-  "url": "http://api.gildman.ru/v1",
+  "url": "http://api.gildman.ru",
   "sampleUrl": "http://api.gildman.ru/v1",
   "template": {
     "forceLanguage": "en"
@@ -10,7 +10,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-07-18T04:03:57.747Z",
+    "time": "2016-07-18T04:17:55.401Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
